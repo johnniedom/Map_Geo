a961a6f3127ad4dd1513b631ae5cfe8788ac1f29
JohnnieDom <johnniedom1@gmail.com>
2023-01-25 23:46:54 +0100

    geo-bug

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import L from './leaflet';
+// import 'leaflet/dist/leaflet.css';
+
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -10,3 +13,35 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+console.log(navigator.geolocation);
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude);
+      console.log(longitude);
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},11.97z`
+      );
+    },
+    function () {
+      alert(`Could not get your position`);
+    }
+  );
+  // THis L is leaflet namespace
+  const map = L.map('map').setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([51.5, -0.09])
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+}
